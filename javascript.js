31b4ec5df0cef7da9cfd6b14704dd92bf7030705
FireDragonSrc <firedragonsrc@gmail.com>
2023-12-10 19:30:06 -0300

    Commit inicial

---
+++ javascript.js
@@ -0,0 +1,17 @@
+/*
+let nome = prompt('Digite o seu nome');
+let idade = prompt('Digite a sua idade');
+let salvarDados = nome == 'Carlos' && idade >= 18 ? document.write('Seu login está certo <3') : document.write('Login incorreto');
+
+function somar(){
+let txt1 = document.getElementById(txtn1)
+let txt2 = document.getElementById(txtn2)
+let res = document.getElementById('res')
+let n1 = Number(txt1.value)
+let n2 = Number(txt2.value)
+let v = n1 + n2
+res.innertext = `A soma entre ${n1} e ${n2} e igual a: ${v}`
+} 
+let nome = prompt('Olá, qual o seu nome?');
+let idade = prompt(`${nome} qual a sua idade?`);
+*/
